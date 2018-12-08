348e683527fd8019199152c5c35ed35f9e50a298
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-12-08 14:04:31 -0800

    added scroll function for nav bar links

--- js/app.js
+++ js/app.js
@@ -4,9 +4,22 @@
 
 $(()=>{
 
+  $('.aboutMeLink').click(()=>{
+    $('html', 'body').animate({
+      scrollTop: $('#aboutMe').position().top}, 1000
+    );
+  });
 
+  $('.contactLink').click(()=>{
+    $('html', 'body').animate({
+      scrollTop: $('#contact').position().top}, 1000
+    );
+  });
 
-
-
+  $('.projectsLink').click(()=>{
+    $('html', 'body').animate({
+      scrollTop: $('#projects').position().top}, 1000
+    );
+  });
 
 })
